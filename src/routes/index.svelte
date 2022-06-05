6d67609f12f65351e0578a6b5588b75a7ca898c2
jeffpohlmeyer <jeffvp@protonmail.ch>
2022-06-05 13:05:32 -0400

    feat: adding button to navigate to blog

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,10 @@
-<h1 class="text-5xl text-blue-500">Welcome to SvelteKit</h1>
-<p class="text-xl text-red-600">
-	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a>
-	to read the documentation
-</p>
+<div class="grid h-screen place-content-center gap-3">
+	<h1 class="text-5xl text-blue-500">Welcome to SvelteKit</h1>
+	<p class="text-xl text-red-600">
+		Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a>
+		to read the documentation
+	</p>
+	<a href="/blog" class="mx-auto rounded-xl bg-amber-700 px-20 py-4 text-white" sveltekit:prefetch>
+		Go to Blog
+	</a>
+</div>
